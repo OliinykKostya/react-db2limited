a0d001281803a8e6c57e539a0294a6bfc5c086ac
OliinykKostya <oliinyk.kostya@gmail.com>
2020-08-19 16:01:07 +0300

    Updates

---
+++ precache-manifest.d0afa2b0218f3a3220da1dd29dbdbf3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d34d177ef262f1581c5e6a99297627",
+    "url": "/react-db2limited/index.html"
+  },
+  {
+    "revision": "e937fe36840b427de4f9",
+    "url": "/react-db2limited/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98c2a2ae10d473b283e2",
+    "url": "/react-db2limited/static/js/2.d780e961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-db2limited/static/js/2.d780e961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e937fe36840b427de4f9",
+    "url": "/react-db2limited/static/js/main.7c606099.chunk.js"
+  },
+  {
+    "revision": "7aca8ca45593c5f5a680",
+    "url": "/react-db2limited/static/js/runtime-main.53058a94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-db2limited/static/media/logo.5d5d9eef.svg"
+  }
+]);
